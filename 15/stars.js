ee285c7fb23e1458910212a8f1c2aca0aab3950d
gresiandra <gresiandra@gmail.com>
2021-06-03 14:48:07 +0800

    print stars done

---
+++ 15/stars.js
@@ -0,0 +1,41 @@
+function printStars(number) {
+
+    let stars = " ";
+
+    for (let i = 1; i < number; i++) {
+
+        for (let j = number; j >= i; j--) {
+            stars += "*"
+            stars += " "
+        }
+        stars += "\n"
+
+        const sisa = number - i;
+        const space = number - sisa;
+
+        for (let k = -1; k < space; k++) {
+            stars += " "
+        }
+        
+    }
+
+    for (let m = number; m >= 1; m--) {
+
+        for (let n = number; n >= m; n--) {
+            stars += "*"
+            stars += " "
+        }
+        stars += "\n"
+
+        const sisa = number - m;
+        const space = number - sisa;
+
+        for (let o = 0; o < space-1; o++) {
+            stars += " "
+        }
+        
+    }
+    console.log(stars);
+}
+
+printStars(prompt('masukan angka: '))
